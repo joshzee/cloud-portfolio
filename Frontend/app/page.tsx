276f234c42e11141860cf70d6dcb897714831057
Joshua Zarazovski <joshuazarazovski@gmail.com>
2025-05-05 08:28:08 +1000

    Update/ New Config for Export and some design tweaks

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,23 +1,33 @@
 import { BlogPosts } from "app/components/posts";
 import { ExperienceList } from "app/components/experience";
-
+import ParticlesBackrgound from "app/components/ParticlesBackrgound";
+import TransitionEffect from "app/components/TransitionEffect";
+import Image from "next/image";
+import ProfilePic from "./images/self.jpg";
 export default function Page() {
 	return (
 		<section>
-			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-				Josh Zarazovski
+			<Image
+				src={ProfilePic}
+				width={120}
+				height={120}
+				alt="Profile Picture"
+				className="profile-img"
+			/>
+			<h1 className="text-2xl font-semibold tracking-tighter home-header align-top justify-center pt-12">
+				Joshua Zarazovski
 			</h1>
 			<p className="mb-4">
-				{`I'm a Cloud enthusiast and AI advocate, finding unmatched efficiency in
-        AWS' wide range of Developer and Operations services. I started off 12+ years ago in ActionScript/Flash Development (RIP)
-        and have worn many hats since then. I now find myself heavily involved in Enterprise and AI Technical Support, currently working mainly with DynamoDB,
-        Cloudwatch, S3, API Gateway and Lambda, which combined, is a great serverless approach to solve the chaleneges that the organisation faces tackling
-        AI hallucinations, prompt input/output and more. When I'm not at work, I enjoy working on my homelab, slowly building upon the container & orchestrator services and 
-        self-hosting as much as I can. I also love learning more about Physiology, Keeping Fit, New Tech, Comics and the latest in Neuroscience.`}
+				{`I'm a Cloud enthusiast and AI advocate, finding unmatched effectivenes and efficiency in
+        AWS' wide range of services for Developers, Operations, QA Testers, Support and many more. I started off 12+ years ago in ActionScript/Flash Development (RIP)
+        and have worn many hats since then. I now find myself heavily involved in Enterprise and AI Technical Support, debugging code and Cloud instrastructure. I'm currently working mainly with DynamoDB,
+        Cloudwatch, S3, API Gateway and Lambda, which combined, is a great serverless approach to solve the challenges that the organisation faces tackling
+        AI hallucinations, Prompt generation and more. When I'm not at work, I enjoy jigging around my Homelab, Exercise Physiology, Keeping Fit, New Tech, Comics and playing with my Dog.`}
 			</p>
 			<div className="my-8">
 				<ExperienceList />
 			</div>
+			<ParticlesBackrgound />
 		</section>
 	);
 }
