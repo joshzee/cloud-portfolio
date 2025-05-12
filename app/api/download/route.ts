fc37ad66d797f1cb156b84fb1e1e545da74b562a
Joshua Zarazovski <joshuazarazovski@gmail.com>
2025-05-12 13:32:14 +1000

    Update download route

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 export async function GET(request: Request) {
-	const filePath = path.join(process.cwd(), "app/public", "jz-resume.pdf"); // file name and location
+	const filePath = path.join(process.cwd(), "/", "jz-resume.pdf"); // file name and location
 	const data = await fs.readFileSync(filePath);
 
 	return new Response(data, {
