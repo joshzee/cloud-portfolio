95909cba01b1d7c33b415afaa2fcefe9e8b6fdc0
Joshua Zarazovski <joshuazarazovski@gmail.com>
2025-05-12 13:15:16 +1000

    Add Download file route for resume.pdf

---
+++ app/api/download/route.ts
@@ -0,0 +1,13 @@
+import fs from "fs";
+import path from "path";
+
+export async function GET(request: Request) {
+	const filePath = path.join(process.cwd(), "app/public", "jz-resume.pdf"); // file name and location
+	const data = await fs.readFileSync(filePath);
+
+	return new Response(data, {
+		headers: {
+			"Content-Type": "application/pdf", // hardcoded mime type, you can change it if you want
+		},
+	});
+}
