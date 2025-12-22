1c20c2cbc7bb355a982d9939288607d7868d5077
Joshua Zarazovski <hi@joshcodes.me>
2025-12-22 12:40:16 +1100

    Grammar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -18,9 +18,14 @@ export default function Page() {
 				Joshua Zarazovski
 			</h1>
 			<p className="mb-4">
-				{`I’m a hands-on Technical Support Engineer transitioning toward DevOps, combining enterprise support experience at Fastly with active learning through KubeCraft’s real-world internship program. I troubleshoot complex networking and API issues daily using tools like Wireshark, Google Cloud, and proprietary network management platforms, while building skills across Linux, Docker/Kubernetes, GitLab/GitHub CI/CD, Terraform, monitoring, and IaC best practices.
-
-				I’m working toward certifications including AWS Solutions Architect and CKA, and developing a public portfolio through guided projects and mentoring from senior engineers. Outside of work, I stay curious—experimenting in my homelab, learning new tech, keeping fit, exploring exercise physiology, reading comics, and spending time with my dog. I bring operational experience, learning hunger, and a fresh, growth-focused mindset, and I’m actively pursuing opportunities to contribute and grow in DevOps/SRE platforms.`}
+				I’m a hands-on Technical Support Engineer transitioning toward DevOps, combining enterprise support experience at Fastly with active learning 
+				through KubeCraft’s real-world internship program. I troubleshoot complex networking and API issues daily using tools like Wireshark, Google Cloud, 
+				and proprietary network management platforms, while building skills across Linux, Docker/Kubernetes, GitLab/GitHub CI/CD, Terraform, monitoring, and IaC best practices.
+				<br/><br/>
+				I’m working toward certifications including AWS Solutions Architect and CKA, and developing a public portfolio through guided projects and mentoring from 
+				senior engineers. Outside of work, I stay curious—experimenting in my homelab, learning new tech, keeping fit, exploring exercise physiology, reading comics, and 
+				spending time with my dog. I bring operational experience, learning hunger, and a fresh, growth-focused mindset, and I’m actively pursuing opportunities to 
+				contribute and grow in DevOps/SRE platforms.`
 			</p>
 			<div className="my-8">
 				<ProjectList />
